52bf7c695c9516545550b88dcec337224157cb53
Anreet Kaur <anreet.kaur@sheridancollege.ca>
2024-03-02 23:24:33 -0500

    Initial set up

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          //logo
+          Icon(
+            Icons.lock,
+            size: 100,
+          ),
+
+          //welcome back text
+
+          //username field
+
+          //password field
+
+          //forgot password
+
+          //sign in button
+
+          //or continue with
+
+          //google + apple sign in button
+
+          //not a member? register now
+        ],
+      ),
+    );
+  }
+}
